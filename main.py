3816efd3cec0a4184f7da0e764868a0ced038d7f
Ianzani <ianzanisirbone@gmail.com>
2024-04-10 21:53:57 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import requests
+import json
+
+token = "d5T8iVFEc66CpdkdUpPTgr"
+api_root = "https://brapi.dev/api/quote/"
+
+def main():
+    stocks = ['PETR4F']
+    stocks.append('MGLU3')
+
+    params = {
+        "token" : token
+    }
+    
+    for stock in stocks:
+        r = requests.get(api_root + stock, params = params)
+        
+        data = json.loads(r.text)['results'][0]
+        print(f"{data['symbol']} : {data['regularMarketPrice']}")
+
+if __name__ == "__main__":
+    main()
